eb76dc5c8bf115cedd2a90d6b9d9a9dc63449561
tungnthust <thanhtungbka1@gmail.com>
2024-01-17 14:01:43 +0700

    update num in_channels

--- guided_diffusion/script_util.py
+++ guided_diffusion/script_util.py
@@ -180,9 +180,9 @@ def create_model(
 
     return UNetModel(
         image_size=image_size,
-        in_channels=3,
+        in_channels=1,
         model_channels=num_channels,
-        out_channels=(3 if not learn_sigma else 6),
+        out_channels=(1 if not learn_sigma else 2),
         num_res_blocks=num_res_blocks,
         attention_resolutions=tuple(attention_ds),
         dropout=dropout,
@@ -229,7 +229,7 @@ def create_classifier(
 
     return EncoderUNetModel(
         image_size=image_size_inference,
-        in_channels=3,
+        in_channels=1,
         model_channels=classifier_width,
         out_channels=1000,
         num_res_blocks=classifier_depth,
